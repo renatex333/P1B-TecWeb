10a917b9dd206b04fc107d15c98d1a68a6c17283
Renato Laffranchi Falcão <renatolf1@al.insper.edu.br>
2022-03-24 09:15:33 -0300

    CSS certo assim mesmo

--- notes/views.py
+++ notes/views.py
@@ -8,8 +8,8 @@ def index(request):
         title = request.POST.get('titulo')
         content = request.POST.get('detalhes')
         # TAREFA: Utilize o title e content para criar um novo Note no banco de dados
-        # new_note = Note(title=title, content=content)
-        # new_note.save()
+        new_note = Note(title=title, content=content)
+        new_note.save()
         return redirect('index')
     else:
         all_notes = Note.objects.all()
