160912d7174d40dd5807d6ec0b0d62dbceceba2e
Renato Laffranchi Falcão <66042074+renatex333@users.noreply.github.com>
2023-11-13 20:56:10 -0300

    Update settings.py

--- getit/settings.py
+++ getit/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yum+==afhe%!%5-cx1hvu95@!ff$-e61)=)3v#7_uh7d3p%h*('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
